9361f71ecc199a70cf8c70feac61e1778278e3f2
diendk <dinhkhacdien1009@gmail.com>
2025-09-11 17:30:46 +0700

    refactor dependency injection

--- src/NexusPM.API/DependencyInjection.cs
+++ src/NexusPM.API/DependencyInjection.cs
@@ -1,8 +1,16 @@
 ﻿// Copyright (c) YourCompany. All rights reserved.
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using NexusPM.API.Authorization;
 using NexusPM.Application.Abstractions.Security;
+using NexusPM.Domain.Enums;
 using NexusPM.Infrastructure.Boot;
+using NexusPM.Infrastructure.Identity.Configurations;
+using NexusPM.Infrastructure.Identity.Interceptors;
+using NexusPM.Infrastructure.Identity.Services.KeyMaterial;
 
 namespace NexusPM.API;
 
@@ -23,6 +31,65 @@ public static class DependencyInjection
         services.AddScoped<ICurrentUser, CurrentUser>();
 
         services.AddScoped<IAuthorizationHandler, TenantRoleHandler>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds JWT authentication services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to which the authentication services will be added.</param>
+    /// <param name="configuration">The configuration containing JWT settings.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddJWTAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentityCore<ApplicationUser>(o =>
+                {
+                    o.Password.RequiredLength = 8;
+                    o.Password.RequireDigit = true;
+                    o.Password.RequireLowercase = true;
+                    o.Password.RequireUppercase = true;
+                    o.Password.RequireNonAlphanumeric = true;
+                    o.User.RequireUniqueEmail = true;
+                    o.SignIn.RequireConfirmedEmail = true;
+                    o.Lockout.MaxFailedAccessAttempts = 5;
+                    o.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                })
+                    .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                    .AddSignInManager<SignInManager<ApplicationUser>>()
+                    .AddDefaultTokenProviders();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromSeconds(30),
+                    NameClaimType = JwtRegisteredClaimNames.Sub,
+                };
+
+                o.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = async context =>
+                    {
+                        var keyProvider = context.HttpContext.RequestServices.GetRequiredService<IKeyMaterialProvider>();
+                        var validationKeys = await keyProvider.GetValidationKeysAsync();
+                        o.TokenValidationParameters.IssuerSigningKeys = validationKeys;
+                    },
+                };
+            });
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy("TenantOwner", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Owner)))
+            .AddPolicy("TenantAdmin", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Admin)))
+            .AddPolicy("TenantMember", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Member)))
+            .AddPolicy("TenantBilling", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Billing)));
 
         return services;
     }
--- src/NexusPM.API/Program.cs
+++ src/NexusPM.API/Program.cs
@@ -1,17 +1,8 @@
 // Copyright (c) YourCompany. All rights reserved.
 
 using DotNetEnv;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
-using NexusPM.API.Authorization;
 using NexusPM.API.Middlewares;
-using NexusPM.Domain.Enums;
 using NexusPM.Infrastructure;
-using NexusPM.Infrastructure.Identity.Configurations;
-using NexusPM.Infrastructure.Identity.Interceptors;
-using NexusPM.Infrastructure.Identity.Services.KeyMaterial;
 
 namespace NexusPM.API;
 
@@ -37,53 +28,7 @@ public class Program
 
         builder.Services.AddAPIService();
 
-        builder.Services.AddIdentityCore<ApplicationUser>(o =>
-        {
-            o.Password.RequiredLength = 8;
-            o.Password.RequireDigit = true;
-            o.Password.RequireLowercase = true;
-            o.Password.RequireUppercase = true;
-            o.Password.RequireNonAlphanumeric = true;
-            o.User.RequireUniqueEmail = true;
-            o.SignIn.RequireConfirmedEmail = true;
-            o.Lockout.MaxFailedAccessAttempts = 5;
-            o.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-        })
-            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
-            .AddSignInManager<SignInManager<ApplicationUser>>()
-            .AddDefaultTokenProviders();
-
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(o =>
-            {
-                o.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
-                    ValidateIssuerSigningKey = true,
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromSeconds(30),
-                    NameClaimType = JwtRegisteredClaimNames.Sub,
-                };
-
-                o.Events = new JwtBearerEvents
-                {
-                    OnTokenValidated = async context =>
-                    {
-                        var keyProvider = context.HttpContext.RequestServices.GetRequiredService<IKeyMaterialProvider>();
-                        var validationKeys = await keyProvider.GetValidationKeysAsync();
-                        o.TokenValidationParameters.IssuerSigningKeys = validationKeys;
-                    },
-                };
-            });
-
-        builder.Services.AddAuthorizationBuilder()
-            .AddPolicy("TenantOwner", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Owner)))
-            .AddPolicy("TenantAdmin", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Admin)))
-            .AddPolicy("TenantMember", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Member)))
-            .AddPolicy("TenantBilling", p => p.Requirements.Add(new TenantRoleRequirement(TenantRole.Billing)));
+        builder.Services.AddJWTAuthentication(builder.Configuration);
 
         builder.Services.AddHttpContextAccessor();
 
